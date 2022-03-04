c172a778f9e513c4643df835ef9911af82c045e4
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-05 00:45:44 +0100

    correzione snak 3 blocco 3

--- snak-3/js/script.js
+++ snak-3/js/script.js
@@ -8,18 +8,34 @@ Se sono pari li stampo nell'div "rosso",
 se sono dispari li stampo nell'div "verde".
  *
  */
+//!Creo i miei 2 Div dove all' interno stamperò i numeri
+const redDiv = document.createElement("div");
+const greenDiv = document.createElement("div");
 
-const firstDiv = document.createElement("div");
+//!Aggiungo l'ID ai 2 div creati
+redDiv.id = "red";
+greenDiv.id = "green";
 
-firstDiv.classList.add("red");
+//!Li inserisco nell'HTML con .append
+document.querySelector("main").append(redDiv, greenDiv);
 
-document.querySelector("main").append(firstDiv);
+//!Array con N numeri
+let numberArray = [12,3,40,67,52,89,1,3,23,16,77,31,33,54,122];
 
-// console.log(firstDiv);
+for (let i = 0; i < numberArray.length; i = i + 1){
+    if (isEven(numberArray[i])){
+        redDiv.innerHTML += numberArray[i] + ", ";
+    } else{
+        greenDiv.innerHTML += numberArray[i] + ", ";
+    }
+}
 
-const secondDiv = document.createElement("div");
-
-secondDiv.classList.add("green");
-
-document.querySelector("main").append(secondDiv);
-
+//!Funzione che fa il check del numero se è pari o dispari 
+//!Se è PARI torna TRUE se è DISPARI torna FALSE
+function isEven(number){
+    if(number % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
